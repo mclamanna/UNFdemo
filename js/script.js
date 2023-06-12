332d4103c185d1e730fbb82101644eb451e6dd31
Michael La Manna <michael@mlamanna.com>
2023-06-11 22:20:16 -0400

    update

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const dropdown = document.querySelectorAll(".dropdown");
+const dropdownToggle = document.querySelectorAll(".dropdown-toggle");
+const dropdownMenu = document.querySelectorAll(".dropdown-menu");
+const showClass = "show";
+
+function toggleDropdown() {
+  const thisDropdown = this;
+  thisDropdown.classList.add(showClass);
+  thisDropdown.querySelector(".dropdown-toggle").setAttribute("aria-expanded", "true");
+  thisDropdown.querySelector(".dropdown-menu").classList.add(showClass);
+}
+
+function hideDropdown() {
+  const thisDropdown = this;
+  thisDropdown.classList.remove(showClass);
+  thisDropdown.querySelector(".dropdown-toggle").setAttribute("aria-expanded", "false");
+  thisDropdown.querySelector(".dropdown-menu").classList.remove(showClass);
+}
+
+function handleWindowSize() {
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    dropdown.forEach((item) => {
+      item.addEventListener("mouseenter", toggleDropdown);
+      item.addEventListener("mouseleave", hideDropdown);
+    });
+  } else {
+    dropdown.forEach((item) => {
+      item.removeEventListener("mouseenter", toggleDropdown);
+      item.removeEventListener("mouseleave", hideDropdown);
+    });
+  }
+}
+
+window.addEventListener("load", handleWindowSize);
+window.addEventListener("resize", handleWindowSize);
